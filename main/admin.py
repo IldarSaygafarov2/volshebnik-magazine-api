fedfe0432ec590f34e4b36dbe44051aaf812e1b3
IldarSaygafarov2 <sayildar17@gmail.com>
2025-03-28 12:53:12 +0500

    added unfold admin

--- main/admin.py
+++ main/admin.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
-
+from unfold.admin import ModelAdmin
 from . import models
 
 
 @admin.register(models.Catalog)
-class CatalogAdmin(admin.ModelAdmin):
+class CatalogAdmin(ModelAdmin):
     list_display = ['pk', 'name']
     search_fields = ['name']
     list_display_links = ['pk', 'name']
 
 
 @admin.register(models.Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(ModelAdmin):
     list_display = ['pk', 'name', 'catalog']
     list_display_links = ['pk', 'name']
     list_editable = ['catalog']
@@ -21,17 +21,17 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 @admin.register(models.PublishingHouse)
-class PublishingHouseAdmin(admin.ModelAdmin):
+class PublishingHouseAdmin(ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
 
 @admin.register(models.Series)
-class SeriesAdmin(admin.ModelAdmin):
+class SeriesAdmin(ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
 
 @admin.register(models.Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(ModelAdmin):
     list_display = ['pk', 'name', 'price', 'in_stock', 'barcode']
     list_display_links = ['pk', 'name']
     list_editable = ['price', 'in_stock']
--- shop/settings.py
+++ shop/settings.py
@@ -17,6 +17,13 @@ ALLOWED_HOSTS = [
 ]
 
 INSTALLED_APPS = [
+    "unfold",  # before django.contrib.admin
+    "unfold.contrib.filters",  # optional, if special filters are needed
+    "unfold.contrib.forms",  # optional, if special form elements are needed
+    "unfold.contrib.inlines",  # optional, if special inlines are needed
+    "unfold.contrib.import_export",  # optional, if django-import-export package is used
+    "unfold.contrib.guardian",  # optional, if django-guardian package is used
+    "unfold.contrib.simple_history",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -108,3 +115,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+UNFOLD = {
+    'SITE_TITLE': 'Volshebnik-shop',
+    'SITE_HEADER': 'Volshebnik-shop',
+    'SITE_SUBHEADER': 'admin-panel',
+}
