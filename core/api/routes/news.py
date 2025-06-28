e262b254b6e47e441e70b7ea0ee497a39c227288
IldarSaygafarov2 <sayildar17@gmail.com>
2025-06-28 09:33:44 +0500

    added fixes

--- core/api/routes/news.py
+++ core/api/routes/news.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest, Http404
 from ninja import Router
 
 from core.api.schemas.news import NewsPostSchema
@@ -10,3 +11,13 @@ router = Router(tags=["News"])
 def get_news(request):
     news = NewsPost.objects.filter(is_visible=True).order_by('-created_at')
     return news
+
+
+@router.get('/news/{news_slug}', response=NewsPostSchema)
+def get_news_detail(request: HttpRequest, news_slug: str):
+    detail = NewsPost.objects.filter(slug=news_slug)
+    if not detail.exists():
+        raise Http404()
+    return detail.first()
+
+
