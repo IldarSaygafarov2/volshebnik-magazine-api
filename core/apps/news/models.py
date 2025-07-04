f68bfe3dd421b7801ad1fd5a321e0a6fc5093f5c
IldarSaygafarov2 <sayildar17@gmail.com>
2025-07-04 16:55:45 +0500

    added fixes

--- core/apps/news/models.py
+++ core/apps/news/models.py
@@ -4,7 +4,8 @@ class NewsPost(models.Model):
     title = models.CharField(max_length=200, verbose_name='название')
     slug = models.SlugField(verbose_name='Короткая ссылка', help_text='Данное поле заполнять не нужно')
     content = models.TextField(verbose_name='описание')
-    preview = models.ImageField(verbose_name='фото', upload_to='news/', null=True, blank=True)
+    preview = models.ImageField(verbose_name='фото', upload_to='news/', null=True, blank=True,
+                                default='placeholder.jpg')
     is_visible = models.BooleanField(verbose_name='Показывать на главной стронице?', default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
