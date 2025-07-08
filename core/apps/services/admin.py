7f8675504337b3bf36dd3f8adde4b2c6e062ea21
IldarSaygafarov2 <sayildar17@gmail.com>
2025-07-08 23:00:40 +0500

    changed default django admin to unfold admin on services app

--- core/apps/services/admin.py
+++ core/apps/services/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 
 from .models import Service
 
 
 @admin.register(Service)
-class ServiceAdmin(admin.ModelAdmin):
+class ServiceAdmin(ModelAdmin):
     list_display = ['id', 'title', 'is_visible']
     list_display_links = ['id', 'title']
     list_editable = ['is_visible']
